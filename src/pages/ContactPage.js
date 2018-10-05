33618e577f71b00f30fd33d667de60fa5f43a35e
Tanner <tanner.crockett@gmail.com>
2018-10-05 14:45:59 -0600

    change contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -6,6 +6,7 @@ class ContactPage extends Component {
     return (
       <div>
         <h1>CONTACT PAGE</h1>
+        <p>making a new change to my contact page</p>
         <NameForm />
       </div>
     );
